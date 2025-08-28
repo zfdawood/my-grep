097935f76b000e1fef6ed17d8514e2fe3245f6f3
zfdawood <89550184+zfdawood@users.noreply.github.com>
2025-08-28 14:28:19 -0400

    iterator methods

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,15 @@
-use std::{env, fs, process, error::Error};
-use my_grep::search;
+use std::{env, process};
+use my_grep::{Config, exec};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config= Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config= Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = exec(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
-}
-
-fn exec(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)?;
-
-    for line in search(&config.query, &contents) {
-        println!("{line}");
-    }
-
-    Ok(())
 }
